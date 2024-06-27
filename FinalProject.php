55df6b34eecff87b20af8905a582d6248e4b1c27
athenalateral <elemieu@siue.edu>
2024-06-27 18:58:31 -0500

    Create FinalProject.php

---
+++ FinalProject.php
@@ -0,0 +1,7 @@
+<form action="/action_page.php" method="post">
+  <label for="lon">Longitude:</label>
+  <input type="text" id="lon" name="lon"><br><br>
+  <label for="lat">Latitude:</label>
+  <input type="text" id="lat" name="lat"><br><br>
+  <input type="submit" value="Submit">
+</form>
