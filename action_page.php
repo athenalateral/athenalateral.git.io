238b9dd0e25abf088b7fe2c6ebc817cbe8f86d6f
athenalateral <elemieu@siue.edu>
2024-06-27 21:20:08 -0500

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -1,14 +1,13 @@
 <!--Weather form request-->
 <?php
 
-if($_SERVER("REQUEST_METHOD")=="POST")
+if ($_SERVER["REQUEST_METHOD"] == "POST")
 {
-  $lon=$_POST("lon");
-	$lat=$_POST("lat");
+  $lon = $_POST["lon"];
+	$lat = $_POST["lat"];
 
-  echo "<p>longitude is: </p><br>";
-  echo "<p>latitude is: </p>";
+  echo "<p>longitude is: $lon</p><br>";
+  echo "<p>latitude is: $lat</p>";
   
 }
-
 ?>
