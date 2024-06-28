2052af630083f8ebc814f47d4c670632dddffb2b
athenalateral <elemieu@siue.edu>
2024-06-27 23:21:43 -0500

    added json read and decode

--- action_page.php
+++ action_page.php
@@ -12,10 +12,24 @@
     
         echo "<p>Longitude is: $lon</p><br>";
         echo "<p>Latitude is: $lat</p>";
+
+        //My hope is that this will pull the info from the website but probably not
+        $url = 'https://api.weather.gov/gridpoints/TOP/$lat,$lon/forecast';
+
+        $json = file_get_contents($url);
+
+        $data = json_decode($json, true);
+
+        if($data) {
+            echo "<h2>Weather Data:</h2>";
+
+        }
+        else {
+            echo "<p>you did it wrong :(</p>";
+        }
+
     }
 ?>
         
 </body>
 </html>
-
-
