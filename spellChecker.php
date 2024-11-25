e45fb886e32723f6a57d8b27e7593e748b5ce282
athenalateral <gumplesteen21@gmail.com>
2024-11-24 20:24:33 -0600

    Add files via upload

---
+++ spellChecker.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Spell Checker</title>
+</head>
+<body>
+    <h1>this is a test to see what's happening</h1>
+    <a href="https://athenalateral.github.io/athenalateral.git.io/">Back to Homepage</a>
+</body>
+</html>
